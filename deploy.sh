9de5c992c5e8d6b79c945790eee54b23bbfc75f0
camilo-restrepo <elpisco@gmail.com>
2018-10-27 20:00:46 -0500

    Migrated to airflow.

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+target_dir=/Volumes/Files/workspace/airflow/dags/creditCardProcessor/
+source_dir=/Volumes/Files/workspace/creditCardProcessor
+
+cp ${source_dir}/main.py ${target_dir}
+
+
